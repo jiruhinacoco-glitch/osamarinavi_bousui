4fc522e5af4ee822b5ef4f02144dbf9ccb89885d
jiruhinacoco-glitch <jiru.hina.coco@gmail.com>
2026-09-23 14:58:20 +0900

    工法別受注情報に防水アイコン4種類を追加

--- sw.js
+++ sw.js
@@ -11,9 +11,10 @@
      ので、こちらの修正が確実に端末へ届く。
      ★ページを直したら、必ずこの番号を1つ上げること。
    ============================================================ */
-const CACHE = 'nn-cache-v541';
+const CACHE = 'nn-cache-v542';
 
 const ASSETS = [
+  './icons/kou_enbi.png','./icons/kou_ure.png','./icons/kou_frp.png','./icons/kou_as.png',
   './visible_face.js',
   './record_view.js','./record_excel.js',
   './document_register.js',
